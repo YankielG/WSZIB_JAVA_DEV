2329b9e3917abfc96fcd68ee7ad4e8376e246ff0
YankielG <Decybelq@o2.pl>
2020-04-28 18:59:12 +0200

    konfiguracja ms sql

--- src/main/java/pl/edu/wszib/jwd/java_dev/JavaDevApplication.java
+++ src/main/java/pl/edu/wszib/jwd/java_dev/JavaDevApplication.java
@@ -2,8 +2,10 @@ package pl.edu.wszib.jwd.java_dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db.properties")
 public class JavaDevApplication {
 
     public static void main(String[] args) {
