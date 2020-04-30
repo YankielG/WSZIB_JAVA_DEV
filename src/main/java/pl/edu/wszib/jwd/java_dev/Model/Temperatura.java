ec22cc7a0852d6bc71834f3edfd3905441644293
YankielG <Decybelq@o2.pl>
2020-04-30 17:36:57 +0200

    Dodanie Temperatura

---
+++ src/main/java/pl/edu/wszib/jwd/java_dev/Model/Temperatura.java
@@ -0,0 +1,69 @@
+package pl.edu.wszib.jwd.java_dev.Model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="temperatura")
+public class Temperatura {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Integer id;
+
+    private Date data;
+    private float temperatura;
+    private String rodzaj;
+    private String uwagi;
+
+    public Temperatura() {
+    }
+
+    public Temperatura(Integer id, Date data, float temperatura, String rodzaj, String uwagi) {
+        this.id = id;
+        this.data = data;
+        this.temperatura = temperatura;
+        this.rodzaj = rodzaj;
+        this.uwagi = uwagi;
+    }
+
+     public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public String getRodzaj() {
+        return rodzaj;
+    }
+
+    public void setRodzaj(String rodzaj) {
+        this.rodzaj = rodzaj;
+    }
+
+    public String getUwagi() {
+        return uwagi;
+    }
+
+    public void setUwagi(String uwagi) {
+        this.uwagi = uwagi;
+    }
+}
---
+++ src/main/java/pl/edu/wszib/jwd/java_dev/dao/TemperaturaDao.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib.jwd.java_dev.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.edu.wszib.jwd.java_dev.Model.Temperatura;
+
+@Repository
+public interface TemperaturaDao extends CrudRepository <Temperatura,Integer> {
+Temperatura findByTemperatura(String temeratura);
+}
