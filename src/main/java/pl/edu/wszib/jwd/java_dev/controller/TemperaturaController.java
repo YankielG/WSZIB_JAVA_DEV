a8ac5b429a25667f93e8071dc2b9e1ad82472f76
YankielG <Decybelq@o2.pl>
2020-05-04 09:47:29 +0200

    test tabela

--- src/main/java/pl/edu/wszib/jwd/java_dev/controller/TemperaturaController.java
+++ src/main/java/pl/edu/wszib/jwd/java_dev/controller/TemperaturaController.java
@@ -1,14 +1,35 @@
 package pl.edu.wszib.jwd.java_dev.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@PropertySource("classpath:app.properties")
 public class TemperaturaController {
 
+    @Value("${temperatura.wstaw.tytul}")
+    private String tytul;
+
     @GetMapping("temperatura_wstaw")
-    public String wstawianieTemperatury (){
+    public String wstawianieTemperatury(Model model) {
+
+
+        String[][] colors = {
+                {"red","blue","purple","teal"},
+                {"black","orange", "yellow", "green"},
+                {"gray","silver", "olive", "lime"},
+                {"navy","lime", "aqua", "fuchsia"}
+        };
+        model.addAttribute("colors", colors);
+        model.addAttribute("tytul", tytul);
         return "temperatura_wstaw";
+
+
+
+
     }
 
 }
