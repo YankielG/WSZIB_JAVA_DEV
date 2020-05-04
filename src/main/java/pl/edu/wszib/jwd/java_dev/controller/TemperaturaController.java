cdef3881ae8b86ca8e46662d786cd5120c36c664
YankielG <Decybelq@o2.pl>
2020-05-04 18:13:18 +0200

    test widoku

--- src/main/java/pl/edu/wszib/jwd/java_dev/controller/TemperaturaController.java
+++ src/main/java/pl/edu/wszib/jwd/java_dev/controller/TemperaturaController.java
@@ -1,33 +1,48 @@
 package pl.edu.wszib.jwd.java_dev.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.edu.wszib.jwd.java_dev.Model.Temperatura;
+import pl.edu.wszib.jwd.java_dev.dao.TemperaturaDao;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 
 @Controller
 @PropertySource("classpath:app.properties")
 public class TemperaturaController {
 
+    @Autowired
+    TemperaturaDao temperaturaDao;
+
+
     @Value("${temperatura.wstaw.tytul}")
     private String tytul;
 
+//    @GetMapping("temperatura_wstaw")
+//    public String wstawianieTemperatury(Model model) {
+//        model.addAttribute("tytul", tytul);
+//        return "temperatura_wstaw";
+//    }
+
+
     @GetMapping("temperatura_wstaw")
     public String wstawianieTemperatury(Model model) {
-
-
-        String[][] colors = {
-                {"red","blue","purple","teal"},
-                {"black","orange", "yellow", "green"},
-                {"gray","silver", "olive", "lime"},
-                {"navy","lime", "aqua", "fuchsia"}
-        };
-        model.addAttribute("colors", colors);
+        Iterable<Temperatura> temperatura = temperaturaDao.findAll();
+        Collection<Temperatura> temperaturaCollection
+                = (Collection<Temperatura>) temperatura;
+//        Map<long> wykres = temperaturaCollection.stream()
+//                .collect(Collectors.groupingBy(Temperatura::getTemperatura));
+        model.addAttribute("temperatura", temperatura);
         model.addAttribute("tytul", tytul);
         return "temperatura_wstaw";
-
-
 
 
     }
